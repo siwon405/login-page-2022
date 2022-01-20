249d051f06a9c3687116fb194cbe14ce840bcbcd
siwon405 <on.siwon@gmail.com>
2022-01-20 21:49:21 +0900

    DOM활용한 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,2 +1,14 @@
-console.log("hello");
-console.log("hell o");
+const id = document.querySelector("#id"),
+  psword = document.querySelector("#psword"),
+loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+
+    console.log(req);
+}
