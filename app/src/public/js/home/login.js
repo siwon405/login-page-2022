44b68d8e9c0314ed2de48eaaaa7aed0f6a5446fc
siwon405 <on.siwon@gmail.com>
2022-01-20 22:25:52 +0900

    로그인 요청 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -10,5 +10,11 @@ function login() {
         psword: psword.value,
     };
 
-    console.log(req);
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    });
 }
