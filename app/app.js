2448db86b4926f7257dfd2d74fc23ad6465dda70
siwon405 <on.siwon@gmail.com>
2022-01-21 22:42:23 +0900

    백엔드 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 // 모듈
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 
@@ -12,6 +13,9 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use(express.static(`${__dirname}/src/public`));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", home); // use 는 미들 웨어를 등록해주는 메서드
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,12 +1,42 @@
-const hello = (req, res) => {
+
+const output = {
+    hello: (req, res) => {
     res.render("home/index");
+},
+
+    login: (req, res) => {
+    res.render("home/login");
+},
 };
 
-const login = (req, res) => {
-    res.render("home/login");
+const users = {
+    id: ["woorimIT", "노드개발", "siwon"],
+    psword: ["0000", "0000", "001001"],
 };
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+        psword = req.body.psword;
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "아이디와 비밀번호를 확인후 로그인 해주세요",
+        });
+    },
+};
+
 
 module.exports = {
-    hello,
-    login,
+    output,
+    process,
 };
