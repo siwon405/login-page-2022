5e4abdbb3989a4b0a8f127c0e2777e42caabefc4
siwon405 <on.siwon@gmail.com>
2022-01-30 19:51:28 +0900

    dotenv로 환경변수 등록

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host: "login-page.ca9t9upyttvp.ap-northeast-2.rds.amazonaws.com",
-    user: "admin",
-    password: "yallu420401",
-    database: "login_page",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PSWORD,
+    database: process.env.DB_DATABASE,
 });
 
 db.connect();
