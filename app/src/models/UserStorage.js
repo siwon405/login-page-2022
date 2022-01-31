0b203bd678ccdc3be8ade26a90f31a2617fad95f
siwon405 <on.siwon@gmail.com>
2022-01-31 19:41:29 +0900

    login-page 최적화

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -9,7 +9,7 @@ class UserStorage {
 
     static getUserInfo(id) {
         return new Promise((resolve, reject) => {
-            const query = "SELECT * FROM us WHERE id = ?";
+            const query = "SELECT * FROM users WHERE id = ?";
             db.query( query,[id], (err, data) => {
                 if (err) reject(`${err}`);
                 else resolve (data[0]);
